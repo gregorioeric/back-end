d428526f1df6ef1ca9fae2682f20ef517e9a5725
Eric Gregório De Aquino <gregorio.eric@gmail.com>
2024-04-27 21:50:50 +0100

    Criado arquivos index.ejs, articles.ejs, rotas, style.css

--- server.js
+++ server.js
@@ -1,7 +1,20 @@
 import express from 'express';
+import router from './routes/articles.js';
+// import { dirname, join, resolve } from 'path';
 
 const app = express();
 const port = 4500;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
+app.use(express.static('public'));
+
+app.use('/articles', router);
+
+app.get('/', (req, res) => {
+    res.render('index', { articles: 'Programando back-end' });
+})
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`);
